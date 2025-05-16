e07e6f876cc51d7b62aaa93d6fc8a0d63338a3e4
Mohsin6544 <mohsintech186@gmail.com>
2025-05-16 06:38:59 -0700

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// firebase.js
+const firebaseConfig = {
+  apiKey: "AIzaSyAn4Ni3Rt_ekp3HIZPo0hZElyr06yh-eIU",
+  authDomain: "loginsignup12-2fe21.firebaseapp.com",
+  projectId: "loginsignup12-2fe21",
+  storageBucket: "loginsignup12-2fe21.firebasestorage.app",
+  messagingSenderId: "872249143851",
+  appId: "1:872249143851:web:3857e51168604ddb07306e",
+  measurementId: "G-ZE6BDQTPJ5"
+};
+
+firebase.initializeApp(firebaseConfig);
+
